3b905d6fec1101b7255cf72cd29e86b65ce71d6a
denilkv <denil.varkey@gmail.com>
2014-06-27 19:22:42 +0530

    Create menu_data.js

---
+++ js/menu_data.js
@@ -0,0 +1,106 @@
+var menu = [{
+
+"menuInfo":[
+
+        {		
+            "mainmenuclass":"someclassforul", // Enter the CLASS of main menu's wrapping <UL> Tag, if required
+			
+            "submenuclass":"someclassforsubul", // Enter the CLASS of Submenu's wrapping <UL> Tag, if required
+			
+            "mainmenuid":"someidforul", // Enter the ID of main menu's wrapping <UL> Tag, if required
+			
+            "submenuid":"someidforul", // Enter the ID of Submenu's wrapping <UL> Tag, if required
+			
+            "menuitemclass":"someclassforli",	  // Enter the CLASS of main menu's <LI> Tag, if required
+
+			"submenuitemclass":"someclassforsubli",	// Enter the CLASS of sub menu's <LI> Tag, if required
+			
+			"mainanochorclass": "someclassforanchor", // Enter the CLASS of main menu's <A> Tag, if required
+			
+			"subanochorclass": "someclassforsubanchor" // Enter the CLASS of sub menu's <A> Tag, if required
+        }
+
+],
+"menuData":[
+
+        {
+			"name": "name1", 
+			"href": "http://google.com",
+            "title": "title 1",
+            "alt":"alt 1",
+			"id": "",
+			"menuChild":[
+
+				{
+				"name": "subname1",
+				"href": "http://google.com",
+				"title":"title 1",
+				"alt":"sub alt 1",
+				"id": "",
+				},
+
+				{
+				"name": "subname2",
+				"href": "http://yahoo.com",
+				"title":"subtitle 1",
+				"alt":"sub alt 1",
+				"id": "",
+				},				
+			],
+			
+        },
+        {
+			"name": "name2",
+			"href": "http://google.com",
+            "title":"title 1",
+            "alt":"alt 1",
+			"id": "",
+			"menuChild":[
+
+				{
+				"name": "subname1",
+				"href": "http://google.com",
+				"title":"title 1",
+				"alt":"sub alt 1",
+				"id": "",
+				},
+
+				{
+				"name": "subname2",
+				"href": "http://yahoo.com",
+				"title":"subtitle 1",
+				"alt":"sub alt 1",
+				"id": "",
+				},				
+			],
+			
+        },
+        {
+			"name": "name3",
+			"href": "http://google.com",
+            "title":"title 1",
+            "alt":"alt 1",
+			"id": "",
+			"menuChild":[
+
+				{
+				"name": "subname1",
+				"href": "http://google.com",
+				"title":"title 1",
+				"alt":"sub alt 1",
+				"id": "",
+				},
+
+				{
+				"name": "subname2",
+				"href": "http://yahoo.com",
+				"title":"subtitle 1",
+				"alt":"sub alt 1",
+				"id": "",
+				},				
+			],
+			
+        }		
+
+]
+}];
